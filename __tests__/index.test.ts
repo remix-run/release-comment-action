622222d2a0f4f143e0ae404492de89ad8e319082
Logan McAnsh <logan@mcan.sh>
2023-09-21 19:16:14 -0400

    test: update test
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,38 +1,50 @@
 import { execaSync } from "execa";
-import { expect, test } from "vitest";
+import { afterAll, beforeAll, expect, test } from "vitest";
 
 import { findMergedPRs, getCommits, getTags } from "../src/lib.js";
 
-let RANDOM_STRING = Math.random().toString(36).substring(7);
-let TMP_DIR = `nightly-release-test-${RANDOM_STRING}`;
-
 //////////////////////////////////////////////// SET UP ////////////////////////////////////////////////
-// clone the repo
-execaSync(
-  "git",
-  ["clone", "https://github.com/mcansh/nightly-release-test", TMP_DIR],
-  { stdio: "inherit" },
-);
-// fetch git tags
-execaSync("git", ["fetch", "--tags"], { stdio: "inherit" });
-process.chdir(TMP_DIR);
+let OWNER = `remix-run` as const;
+let REPO = `remix` as const;
+let REPOSITORY = `${OWNER}/${REPO}` as const;
+let PACKAGE_TO_TRACK = `remix` as const;
+let RANDOM_STRING = Math.random().toString(36).substring(7);
+let TMP_DIR = `${REPO}-${RANDOM_STRING}`;
+
+let cwd;
+
+beforeAll(() => {
+  // clone the repo
+  execaSync("git", ["clone", `https://github.com/${REPOSITORY}`, TMP_DIR], {
+    stdio: "inherit",
+  });
+  // fetch git tags
+  execaSync("git", ["fetch", "--tags"], { stdio: "inherit" });
+  cwd = process.cwd();
+  process.chdir(TMP_DIR);
+});
+
+afterAll(() => {
+  process.chdir(cwd);
+  execaSync("rm", ["-rf", TMP_DIR], { stdio: "inherit" });
+});
 
 //////////////////////////////////////////////// TESTS ////////////////////////////////////////////////
 test("the whole shooting match", async () => {
   execaSync("git", ["fetch", "--tags"], { stdio: "inherit" });
-  let tags = await getTags("@mcansh/nightly-release-test", false);
+  let tags = await getTags(PACKAGE_TO_TRACK, false);
 
   expect(tags).toEqual({
-    latest: { raw: "@mcansh/nightly-release-test@1.3.2", clean: "1.3.2" },
-    previous: { raw: "@mcansh/nightly-release-test@1.3.1", clean: "1.3.1" },
+    latest: { raw: `remix@2.0.1`, clean: `2.0.1` },
+    previous: { raw: `remix@2.0.0`, clean: `2.0.0` },
     isNightly: false,
     isStable: true,
     isPreRelease: false,
   });
 
   let commits = await getCommits(tags.previous, tags.latest, "./packages");
-  expect(commits).toHaveLength(3);
+  expect(commits).toHaveLength(22);
 
   let prs = await findMergedPRs(commits);
-  expect(prs).toHaveLength(1);
+  expect(prs).toHaveLength(16);
 });
